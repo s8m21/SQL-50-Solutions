8706ee2409b8d0ffe15b7ae0f29b930fb7a26b01
Spandan Maaheshwari <127258950+s8m21@users.noreply.github.com>
2025-01-18 14:31:02 -0500

    Update 1148_Article_Views_I.sql

--- 1148_Article_Views_I.sql
+++ 1148_Article_Views_I.sql
@@ -1,1 +1,11 @@
-SELECT    author_id AS idFROM    viewsWHERE    author_id = viewer_id AND viewer_id >= 1GROUP BY    author_idORDER BY    id ASC;
+SELECT
+    author_id AS id
+FROM
+    views
+WHERE
+    author_id = viewer_id AND viewer_id >= 1
+GROUP BY
+    author_id
+ORDER BY
+    id ASC
+;

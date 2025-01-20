b0a7088d6fbf5e43c10a28f130811cb506514605
Spandan Maaheshwari <127258950+s8m21@users.noreply.github.com>
2025-01-20 17:21:17 -0500

    Update 1174_Immediate_Food_Delivery_II.sql

--- 1174_Immediate_Food_Delivery_II.sql
+++ 1174_Immediate_Food_Delivery_II.sql
@@ -1,3 +1,7 @@
 SELECT ROUND(AVG(Delivery.order_date = customer_pref_delivery_date) * 100, 2) AS   immediate_percentage
 FROM Delivery
 WHERE (customer_id, order_date) IN
+
+      (SELECT customer_id, MIN(order_date) AS order_date
+       FROM Delivery 
+       GROUP BY customer_id)
